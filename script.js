103bfc509e72cfa953509e4220ce7ce50fb40d57
AtiqGauri <gauriatiq@gmail.com>
2020-09-21 16:28:06 +0530

    feat(init): basic portfolio website

---
+++ script.js
@@ -0,0 +1,43 @@
+function launch_url(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+}
+
+
+/**
+ * copy email to clipboard onclick
+ */
+document.querySelector(".email").onclick = function(){
+    copyStringToClipboard(".email", "gauriatiq@gmail.com");
+};
+
+
+/**
+ * copy text to clipboard
+ * @param {string} class or id of target element with . & # 
+ * @param {string} string which needs to be copied 
+ */
+function copyStringToClipboard(element, str) {
+    var el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style = {position: 'absolute', left: '-9999px'};
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+    document.querySelector(element).innerHTML = "copied&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;";
+    sleep(500).then(() => {
+        document.querySelector(element).innerHTML = str;
+    })
+    console.log("done");
+}
+
+/**
+ * delay execution of statement for some milliseconds
+ * @param {number} milliseconds 
+ */
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
